5e3926299936c63db175b35f38cd1f5c4e166edb
shu <hhusihai@163.com>
2019-05-27 22:58:24 +0200

    :bulb: chore: add vscode config into gitignore

---
+++ beautifulsoupTests/TestPythonBasic.py
@@ -0,0 +1,37 @@
+# print('What\'s your name, sweetie ?')
+# myName = input()
+# print('Hello cruel ' + myName)
+
+# import sys
+
+# while True:
+#     print('Type exit to exit.')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('You typed ' + response + '.')
+
+# def hello(name):
+#     print('Hello ' + name)
+
+# hello('Atlana')
+
+# def spam(divideBy):
+#     return 42 / divideBy
+
+# try:
+#     print(spam(2))
+#     print(spam(12))
+#     print(spam(0))
+#     print(spam(1))
+# except ZeroDivisionError:
+#     print('Error: Invalid argument')
+
+# print(spam(1))
+
+print('How are you?')
+feeling = input()
+if feeling.lower() == 'great':
+    print('I feel great too.')
+else:
+    print('I hope the rest of your day is good.')
